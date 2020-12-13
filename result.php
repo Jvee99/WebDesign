1aff690b77c76f681b12c6a7727699cb966cbe16
JVEE L. AGUINALDO <70934667+Jvee99@users.noreply.github.com>
2020-12-13 18:44:28 +0800

    Add files via upload

---
+++ result.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once("Home.html");
+include_once("Sample Pictures.html");
+include_once("About Us.html");
+
+$name=$_POST["name"];
+$Address=$_POST["Address"];
+$MobileNumber=$_POST["MobileNumber"];
+$Events=$_POST["Events"];
+
+?>
